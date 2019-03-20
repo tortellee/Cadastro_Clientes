047737c831ec3580cc77dc9ecd610f57f3c28374
tortelle <42223675+tortelle@users.noreply.github.com>
2019-03-20 10:52:10 -0300

    Versão 0.1

---
+++ Cadastro_clientes/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace Cadastro_clientes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btngravar = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbdataehora = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txt4 = new System.Windows.Forms.ComboBox();
+            this.txt5 = new System.Windows.Forms.MaskedTextBox();
+            this.txt6 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome:";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(12, 142);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(233, 20);
+            this.txt1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Endereço:";
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(12, 182);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(233, 20);
+            this.txt2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 205);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Bairro:";
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(12, 221);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(233, 20);
+            this.txt3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 245);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Estado:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 284);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Telefone:";
+            // 
+            // txt7
+            // 
+            this.txt7.Location = new System.Drawing.Point(12, 377);
+            this.txt7.Name = "txt7";
+            this.txt7.Size = new System.Drawing.Size(233, 20);
+            this.txt7.TabIndex = 2;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(9, 322);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Celular:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.White;
+            this.label8.Font = new System.Drawing.Font("Candara", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label8.Location = new System.Drawing.Point(14, 58);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(235, 26);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "CADASTRO DE CLIENTES";
+            // 
+            // btngravar
+            // 
+            this.btngravar.Font = new System.Drawing.Font("Candara", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btngravar.Location = new System.Drawing.Point(12, 412);
+            this.btngravar.Name = "btngravar";
+            this.btngravar.Size = new System.Drawing.Size(233, 52);
+            this.btngravar.TabIndex = 6;
+            this.btngravar.Text = "GRAVAR CADASTRO";
+            this.btngravar.UseVisualStyleBackColor = true;
+            this.btngravar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Candara", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(12, 470);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(233, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "VER CADASTROS";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbdataehora
+            // 
+            this.lbdataehora.AutoSize = true;
+            this.lbdataehora.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbdataehora.Location = new System.Drawing.Point(14, 19);
+            this.lbdataehora.Name = "lbdataehora";
+            this.lbdataehora.Size = new System.Drawing.Size(0, 12);
+            this.lbdataehora.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 361);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Email:";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Candara", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.button4.Location = new System.Drawing.Point(111, 499);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(40, 28);
+            this.button4.TabIndex = 9;
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // txt4
+            // 
+            this.txt4.FormattingEnabled = true;
+            this.txt4.Items.AddRange(new object[] {
+            "SP",
+            "BH",
+            "MG",
+            "RJ",
+            "DF"});
+            this.txt4.Location = new System.Drawing.Point(12, 261);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(49, 21);
+            this.txt4.TabIndex = 10;
+            // 
+            // txt5
+            // 
+            this.txt5.Location = new System.Drawing.Point(13, 301);
+            this.txt5.Mask = "(99) 0000-0000";
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(100, 20);
+            this.txt5.TabIndex = 11;
+            // 
+            // txt6
+            // 
+            this.txt6.Location = new System.Drawing.Point(12, 338);
+            this.txt6.Mask = "(99) 00000-0000";
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(100, 20);
+            this.txt6.TabIndex = 12;
+            this.txt6.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.txt6_MaskInputRejected);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Cadastro_clientes.Properties.Resources.loginn;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(261, 531);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.lbdataehora);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btngravar);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Cadastro de Clientes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btngravar;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbdataehora;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox txt4;
+        private System.Windows.Forms.MaskedTextBox txt5;
+        private System.Windows.Forms.MaskedTextBox txt6;
+    }
+}
+
---
+++ Cadastro_clientes/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Cadastro_clientes
+{
+    public partial class Form1 : Form
+    {
+        DateTime data_hora;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void limparTextBoxes(Control.ControlCollection controles)
+        {
+            //Faz um laço para todos os controles
+            foreach (Control ctrl in controles)
+            {
+                //Se o controle for um MaskedTextBox...
+                if (ctrl is MaskedTextBox) 
+                {
+                    ((MaskedTextBox)(ctrl)).Text = String.Empty;
+                }
+                //Se o controle for um TextBox...
+                else if (ctrl is TextBox)
+                {
+                    ((TextBox)(ctrl)).Text = String.Empty;
+                }
+                //Se o controle for um ComboBox...
+                else if (ctrl is ComboBox)
+                {
+                    ((ComboBox)(ctrl)).Text = String.Empty;
+                }
+            }
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //Faz com que fique impossivel arrastar a borda
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            //Cria a pasta no diretorio X
+            DirectoryInfo CriarPasta = new DirectoryInfo(@"C:\\Banco De Dados\\Cadastro Clientes");
+
+            //Se Caso a pasta existir Returna para o FORM , caso NÃO ele cria
+            if (CriarPasta.Exists)
+            {
+                return;
+
+            }
+            else
+            {
+                CriarPasta.Create();
+            }
+
+           
+        }
+        //Mostra a hora e data em uma LABEL 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            data_hora = DateTime.Now;
+            lbdataehora.Text = data_hora.ToLongDateString() + " / " + data_hora.ToLongTimeString();
+        }
+        //Botão de Sair
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        //Botão para gravar cadastros
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamWriter arquivo;
+            String caminho = "C:\\Banco De Dados\\Cadastro Clientes\\CadastroClientes.txt";
+            arquivo = File.AppendText(caminho);
+            arquivo.WriteLine(" ");
+            arquivo.WriteLine("Nome: " + txt1.Text);           
+            arquivo.WriteLine("Endereço: " + txt2.Text);          
+            arquivo.WriteLine("Bairro: " + txt3.Text);
+            arquivo.WriteLine("Estado: " + txt4.Text);
+            arquivo.WriteLine("Telefone: " + txt5.Text);
+            arquivo.WriteLine("Celular: " + txt6.Text);
+            arquivo.WriteLine("Email: " + txt7.Text);
+            arquivo.WriteLine("Cadastro Realizado: " + lbdataehora.Text + Environment.NewLine);
+            arquivo.WriteLine("=================================================================");
+
+            //Fecha o arquivo apos registrar as informacões no arquivo "Cadastro"
+            arquivo.Close();
+
+            //Mensagem informando que o cadastro foi realizado
+            MessageBox.Show("Cadastrado com sucesso", "Informação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            //Cria um botão com dialogo para continuar , S/N
+            DialogResult dialogResult = MessageBox.Show("Deseja cadastrar outro cliente?", "Pergunta", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            //Cria um IF para o botao acima , se caso SIM , ele returna para o FORM e o lIMPA os campos , caso NÃO ele o FECHA !
+            if (dialogResult == System.Windows.Forms.DialogResult.Yes)
+            {
+                limparTextBoxes(this.Controls);
+                return;
+            }
+
+            //Caso NÃO ele o FECHA !
+            else
+            { 
+                Application.Exit();
+            }
+
+
+        }
+
+        private void txt6_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+        //Botao para mostrar os Cadastros
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string caminho = "C:\\Banco De Dados\\Cadastro Clientes\\CadastroClientes.txt";
+            System.Diagnostics.Process.Start("NOTEPAD", caminho);
+        }
+    }
+    
+}
